66f8b36363b2937c9532278b3d06f76179486151
IgorYudintsev <kakvospitat@gmail.com>
2023-09-23 14:04:42 +0300

    2

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/700.css';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {App} from "@/App"
-import './index.css'
+import './styles/index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
