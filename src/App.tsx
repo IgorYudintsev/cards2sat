5c2026b6e247d9645239c67b132ab91b2644961f
IgorYudintsev <kakvospitat@gmail.com>
2023-09-25 10:11:46 +0300

    3

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 
 //import './App.css'
 
+import {Button} from "@/components/ui/button";
+
 export function App() {
     return(
 
-        <div>Hello</div>
+        <div>
+
+            <Button variant={'primary'}>
+                Primary Button
+            </Button>
+        </div>
 
     )
 }
