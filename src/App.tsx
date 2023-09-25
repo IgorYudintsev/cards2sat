c8013483fbeb8824f136538272f749e80fa20d0d
IgorYudintsev <kakvospitat@gmail.com>
2023-09-25 10:57:31 +0300

    4

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
-
-//import './App.css'
-
 import {Button} from "@/components/ui/button";
 
 export function App() {
     return(
-
         <div>
-
-            <Button variant={'primary'}>
-                Primary Button
-            </Button>
+            <Button variant={'primary'}>Primary Button</Button>
+            <Button as={'a'} href={"http://htmlbook.ru/example/knob.html"} >Link Button</Button>
         </div>
 
     )
