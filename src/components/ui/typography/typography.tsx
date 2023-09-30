d52a1086b3f9f7915965ad137fb421e1e53d219d
IgorYudintsev <kakvospitat@gmail.com>
2023-09-30 21:17:19 +0300

    10 typography -fix

--- src/components/ui/typography/typography.tsx
+++ src/components/ui/typography/typography.tsx
@@ -1,7 +1,7 @@
 import {ComponentPropsWithoutRef, ElementType, ReactNode} from 'react'
 import s from './typography.module.scss'
 
-type VariantType= 'h1' |  'h2'| 'h3'|  'a' | 'span'
+type VariantType= 'large'|'h1' | 'h2'| 'h3'| 'body1'|'subtitle1'|'body2'|'subtitle2'|'caption'|'overline'| 'link1'|'link2'
 export type TypographyProps<T extends ElementType = 'span'> = {
     as: T,
     children: ReactNode,
@@ -10,7 +10,7 @@ export type TypographyProps<T extends ElementType = 'span'> = {
 
 
 
-export const Typography =<T extends ElementType = VariantType> (props: TypographyProps<T>) => {
+export const Typography =<T extends ElementType ='span'> (props: TypographyProps<T>) => {
     const{variant = 'span', className, children, as: Component = 'span', ...rest}=props
     return (
         <Component className={`${s[variant]}  ${className}`} {...rest} >{children}</Component>
