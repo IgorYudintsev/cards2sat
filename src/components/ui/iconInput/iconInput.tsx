f08281c205d4a78df203bda319d7e26e98eb0618
IgorYudintsev <kakvospitat@gmail.com>
2023-11-02 14:34:11 +0300

    22 IconInput

--- src/components/ui/iconInput/iconInput.tsx
+++ src/components/ui/iconInput/iconInput.tsx
@@ -1,7 +1,51 @@
-import {useState} from "react";
-import s from "@/components/ui/input/input.module.scss";
+// import React, { useState } from "react";
+// import s from "@/components/ui/iconInput/iconInput.module.scss";
+// import {Typography} from "@/components";
+//
+// export const IconInput = () => {
+//     const [isFocused, setIsFocused] = useState(false);
+//     //const [isDisabled, setIsDisabled] = useState(false);
+//
+//     const handleFocus = () => {
+//         setIsFocused(true);
+//     };
+//
+//     const handleBlur = () => {
+//         setIsFocused(false);
+//     };
+//
+//     // const handleDisable = () => {
+//     //     setIsDisabled(!isDisabled);
+//     // };
+//
+//     const containerStyles = `
+//     ${s.container}
+//     ${isFocused ? s.focused : ""}
+//
+//   `;
+//
+//     return (
+//         <div className={containerStyles} tabIndex={0} onFocus={handleFocus} onBlur={handleBlur}>
+//             <div className={s.container}>
+//                 <input
+//                     // disabled={disabled}
+//                     className={` ${className} ${styles}`}
+//                     {...rest}
+//                 />
+//                 <img src={search} alt="searchIcon" style={{marginTop: '2px', color: 'white'}}/>
+//             </div>
+//             {/*<Typography as={"span"} variant={'caption'} className={t.error}>{error}</Typography>*/}
+//         </div>
+//     );
+// };
+
+
+//-----------------------------------------------------------
+import React, {useState} from "react";
+import s from "@/components/ui/iconInput/iconInput.module.scss";
 import {Typography} from "@/components";
 import t from "@/components/ui/typography/typography.module.scss";
+import search from "./assets/search.svg";
 
 export type IconInputProps = {
     className?: string
@@ -11,21 +55,48 @@ export type IconInputProps = {
 export const IconInput: React.FC<IconInputProps> = (props) => {
     const {className, disabled, ...rest} = props
 
-    const [error, setError] = useState<string|null>('You have some Error')
+    const [isFocused, setIsFocused] = useState(false);
+     const [error, setError] = useState<string | null>('You have some Error')
+    // const [error, setError] = useState<string | null>(null)
 
-    const styles = `
+    const handleFocus = () => {
+        setIsFocused(true);
+    };
+
+    const handleBlur = () => {
+        setIsFocused(false);
+    };
+
+
+    const stylesContainer = `
+    ${s.container}
+    ${isFocused ? s.containerFocus : ''}
+     ${disabled ? s.containerDisabled : ''}
+      ${error ? s.colorError : s.colorDefault}
+        `
+
+    const stylesInput = `
     ${s.main}
-    ${error ? s.colorError : s.colorDefault}
+   
+
         `
 
     return (
         <>
-            <input
-                disabled={disabled}
-                className={` ${className} ${styles}`}
-                {...rest}
-            />
+            <div
+                className={stylesContainer}
+                onFocus={handleFocus} onBlur={handleBlur}>
+                <input
+                    disabled={disabled}
+                    className={` ${className} ${stylesInput}`}
+                    {...rest}
+                />
+                <img src={search} alt="searchIcon" style={{marginTop: '2px', color: 'white'}}/>
+            </div>
+
+
             <Typography as={"span"} variant={'caption'} className={t.error}>{error}</Typography>
         </>
     )
 }
+
