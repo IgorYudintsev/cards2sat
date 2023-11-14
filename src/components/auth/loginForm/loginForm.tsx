e2577de8f875007386da541ba6f1099da7d17639
IgorYudintsev <kakvospitat@gmail.com>
2023-11-15 01:10:14 +0300

    31 auth-useHookForm

--- src/components/auth/loginForm/loginForm.tsx
+++ src/components/auth/loginForm/loginForm.tsx
@@ -7,18 +7,24 @@ type FormValues = {
     password: string
 }
 export const LoginForm = () => {
-    const { register, handleSubmit } = useForm<FormValues>()
+    // const {register, handleSubmit} = useForm<FormValues>()
+
+    const {
+        control,
+        handleSubmit,
+        formState: { errors }
+    } = useForm<FormValues>();
+
 
     const onSubmit = (data: FormValues) => {
         console.log(data)
     }
     return (
-        <div>
-
-            <SuperInput    icon={"classic"} disabled={false}/>
-            <SuperInput    icon={"password"} disabled={false}/>
-                     <SuperButton/>
-        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <SuperInput   control={control}  name="email" icon={"classic"} disabled={false} label={'email'}/>
+            <SuperInput   control={control}   name="password" icon={"password"} disabled={false}  label={'password'}/>
+            <SuperButton type="submit">Submit</SuperButton>
+        </form>
     );
 };
 
--- src/components/ui/superInput/superInput.tsx
+++ src/components/ui/superInput/superInput.tsx
@@ -5,8 +5,11 @@ import {useState} from "react";
 import s from "./superInput.module.scss";
 import t from "@/components/ui/typography/typography.module.scss";
 import {Typography} from "@/components/ui/typography";
+import {Control, Controller} from "react-hook-form";
 
 export type InputProps = {
+    name:string
+    control: Control<any>;
     label?:string;
     className?: string;
     disabled?: boolean;
@@ -14,10 +17,10 @@ export type InputProps = {
 };
 
 export const SuperInput = (props: InputProps) => {
-    const {className, disabled = true, icon = "search", ...rest} = props;
+    const {control,name,className, disabled = true, icon = "search", ...rest} = props;
     const [isFocused, setIsFocused] = useState(false);
-    //const [error, setError] = useState<string | null>(null);
-    const [error, setError] = useState<string | null>('You have some Error')
+    const [error, setError] = useState<string | null>(null);
+    //const [error, setError] = useState<string | null>('You have some Error')
     const [x, setX] = useState('');
     const[showPassword,setShowPassword]=useState(false)
 
@@ -43,37 +46,134 @@ export const SuperInput = (props: InputProps) => {
      `;
 
     return (
-        <div style={{width: '100%', backgroundColor: 'black'}}>
-            {icon === "search" || icon === "password" ? (
+
                 <>
-                    <div className={stylesContainer} onFocus={handleFocus} onBlur={handleBlur}>
-                        {icon === "search" && <img src={searchPict} alt="searchIcon"/>}
-                        <input
-                            type={showPassword? 'text': "password"}
-                            onChange={(e) => setX(e.currentTarget.value)}
-                            value={x}
-                            disabled={disabled}
-                            className={`${className} ${styles}`}
-                            {...rest}
-                        />
-                        {icon === "search" && x !== '' &&
-                            <img src={xPict} alt="x" className={s.x} onClick={() => setX('')}/>}
-                        {icon === "password" &&
-                            <img src={passwordPict} alt="passwordIcon" className={disabled ? s.icon : ''}/>}
+                    <div style={{width: '100%', backgroundColor: 'black'}}>
+                        {icon === "search" || icon === "password" ? (
+                            <>
+                                <div className={stylesContainer} onFocus={handleFocus} onBlur={handleBlur}>
+                                    {icon === "search" && <img src={searchPict} alt="searchIcon"/>}
+                                    <input
+                                        type={showPassword? 'text': "password"}
+                                        onChange={(e) => setX(e.currentTarget.value)}
+                                        value={x}
+                                        disabled={disabled}
+                                        className={`${className} ${styles}`}
+                                        {...rest}
+                                    />
+                                    {icon === "search" && x !== '' &&
+                                        <img src={xPict} alt="x" className={s.x} onClick={() => setX('')}/>}
+                                    {icon === "password" &&
+                                        <img src={passwordPict} alt="passwordIcon" className={disabled ? s.icon : ''}/>}
+                                </div>
+                                <Typography as="span" variant="caption" className={t.error}>{error}</Typography>
+                            </>
+
+                        ) : (
+                            <div style={{width: '284px'}} onFocus={handleFocus} onBlur={handleBlur}>
+                                <Controller
+                                    name={name}
+                                    control={control}
+                                    // rules={rules}
+                                    // defaultValue={defaultValue}
+                                    render={({ field,fieldState }) => (
+                                <input
+                                    {...field}
+                                    disabled={disabled}
+                                    className={`${className} ${styles}`}
+                                    {...rest}
+                                />
+                                    )}
+                                />
+                                <Typography as="span" variant="caption" className={t.error}>{error}</Typography>
+                            </div>
+                        )}
                     </div>
-                    <Typography as="span" variant="caption" className={t.error}>{error}</Typography>
                 </>
 
-            ) : (
-                <div style={{width: '284px'}} onFocus={handleFocus} onBlur={handleBlur}>
-                    <input
-                        disabled={disabled}
-                        className={`${className} ${styles}`}
-                        {...rest}
-                    />
-                    <Typography as="span" variant="caption" className={t.error}>{error}</Typography>
-                </div>
-            )}
-        </div>
     );
 };
+
+//-----------------------------------------------------------------------------------
+
+// import searchPict from "@/components/ui/iconInput/assets/search.svg";
+// import xPict from "@/components/ui/iconInput/assets/x.svg";
+// import passwordPict from "@/components/ui/iconInput/assets/eye.svg";
+// import {useState} from "react";
+// import s from "./superInput.module.scss";
+// import t from "@/components/ui/typography/typography.module.scss";
+// import {Typography} from "@/components/ui/typography";
+// import {Control} from "react-hook-form";
+//
+// export type InputProps = {
+//     control: Control<any>;
+//     label?:string;
+//     className?: string;
+//     disabled?: boolean;
+//     icon?: "search" | "password" | "classic";
+// };
+//
+// export const SuperInput = (props: InputProps) => {
+//     const {className, disabled = true, icon = "search", ...rest} = props;
+//     const [isFocused, setIsFocused] = useState(false);
+//     const [error, setError] = useState<string | null>(null);
+//     //const [error, setError] = useState<string | null>('You have some Error')
+//     const [x, setX] = useState('');
+//     const[showPassword,setShowPassword]=useState(false)
+//
+//     const handleFocus = () => {
+//         setIsFocused(true);
+//     };
+//
+//     const handleBlur = () => {
+//         setIsFocused(false);
+//     };
+//
+//     const stylesContainer = `
+//   ${s.container}
+//   ${isFocused ? s.containerFocus : ''}
+//   ${disabled ? s.containerDisabled : ''}
+//   ${error ? s.borderError : s.border}
+//   ${error && isFocused ? s.colorError : !error && isFocused ? s.containerFocus : s.colorDefault}
+// `;
+//
+//     const styles = `
+//     ${s.main}
+//     ${error && icon === 'classic' ? s.borderError : icon !== 'classic' ? s.borderBlack : s.borderGrey}
+//      `;
+//
+//     return (
+//         <div style={{width: '100%', backgroundColor: 'black'}}>
+//             {icon === "search" || icon === "password" ? (
+//                 <>
+//                     <div className={stylesContainer} onFocus={handleFocus} onBlur={handleBlur}>
+//                         {icon === "search" && <img src={searchPict} alt="searchIcon"/>}
+//                         <input
+//                             type={showPassword? 'text': "password"}
+//                             onChange={(e) => setX(e.currentTarget.value)}
+//                             value={x}
+//                             disabled={disabled}
+//                             className={`${className} ${styles}`}
+//                             {...rest}
+//                         />
+//                         {icon === "search" && x !== '' &&
+//                             <img src={xPict} alt="x" className={s.x} onClick={() => setX('')}/>}
+//                         {icon === "password" &&
+//                             <img src={passwordPict} alt="passwordIcon" className={disabled ? s.icon : ''}/>}
+//                     </div>
+//                     <Typography as="span" variant="caption" className={t.error}>{error}</Typography>
+//                 </>
+//
+//             ) : (
+//                 <div style={{width: '284px'}} onFocus={handleFocus} onBlur={handleBlur}>
+//                     <input
+//                         disabled={disabled}
+//                         className={`${className} ${styles}`}
+//                         {...rest}
+//                     />
+//                     <Typography as="span" variant="caption" className={t.error}>{error}</Typography>
+//                 </div>
+//             )}
+//         </div>
+//     );
+// };
