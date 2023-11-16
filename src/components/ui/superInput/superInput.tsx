ceda5d57dcedde129430357950106fac66a99792
IgorYudintsev <kakvospitat@gmail.com>
2023-11-16 11:41:17 +0300

    33 auth-useHookForm is done

--- src/components/ui/superInput/superInput.tsx
+++ src/components/ui/superInput/superInput.tsx
@@ -18,12 +18,17 @@ export type InputProps = {
 };
 
 export const SuperInput = (props: InputProps) => {
-    const {name, control ,  className, disabled = true, icon = "search", ...rest} = props;
+    const {name, control, className, disabled = true, icon = "search", ...rest} = props;
     const [isFocused, setIsFocused] = useState(false);
     const [error, setError] = useState<string | null>(null);
     //const [error, setError] = useState<string | null>('You have some Error')
     const [x, setX] = useState('');
+    console.log(x)
     const [showPassword, setShowPassword] = useState(false)
+
+    const onMouseDownHandler = () => {
+        setShowPassword(!showPassword)
+    }
 
     const handleFocus = () => {
         setIsFocused(true);
@@ -62,9 +67,7 @@ export const SuperInput = (props: InputProps) => {
                                 render={({field, fieldState}) => (
                                     <input
                                         {...field}
-                                        type={showPassword ? 'text' : "password"}
-                                        onChange={(e) => setX(e.currentTarget.value)}
-                                        value={x}
+                                        type={!showPassword && icon === "password" ? "password" : 'text'}
                                         disabled={disabled}
                                         className={`${className} ${styles}`}
                                         {...rest}
@@ -74,7 +77,13 @@ export const SuperInput = (props: InputProps) => {
                             {icon === "search" && x !== '' &&
                                 <img src={xPict} alt="x" className={s.x} onClick={() => setX('')}/>}
                             {icon === "password" &&
-                                <img src={passwordPict} alt="passwordIcon" className={disabled ? s.icon : ''}/>}
+                                <img
+                                    src={passwordPict}
+                                    alt="passwordIcon"
+                                    className={disabled ? s.icon : ''}
+                                    onMouseDown={onMouseDownHandler}
+                                    onMouseUp={onMouseDownHandler}
+                                />}
                         </div>
                         <Typography as="span" variant="caption" className={t.error}>{error}</Typography>
                     </>
@@ -103,186 +112,3 @@ export const SuperInput = (props: InputProps) => {
 
     );
 };
-
-//-------------------------------------------------------------------------------------
-
-// import searchPict from "@/components/ui/iconInput/assets/search.svg";
-// import xPict from "@/components/ui/iconInput/assets/x.svg";
-// import passwordPict from "@/components/ui/iconInput/assets/eye.svg";
-// import {useState} from "react";
-// import s from "./superInput.module.scss";
-// import t from "@/components/ui/typography/typography.module.scss";
-// import {Typography} from "@/components/ui/typography";
-// import {Control, Controller} from "react-hook-form";
-//
-// export type InputProps = {
-//     name:string
-//     control: Control<any>;
-//     label?:string;
-//     className?: string;
-//     disabled?: boolean;
-//     icon?: "search" | "password" | "classic";
-// };
-//
-// export const SuperInput = (props: InputProps) => {
-//     const {control='email',name,className, disabled = true, icon = "search", ...rest} = props;
-//     const [isFocused, setIsFocused] = useState(false);
-//     const [error, setError] = useState<string | null>(null);
-//     //const [error, setError] = useState<string | null>('You have some Error')
-//     const [x, setX] = useState('');
-//     const[showPassword,setShowPassword]=useState(false)
-//
-//     const handleFocus = () => {
-//         setIsFocused(true);
-//     };
-//
-//     const handleBlur = () => {
-//         setIsFocused(false);
-//     };
-//
-//     const stylesContainer = `
-//   ${s.container}
-//   ${isFocused ? s.containerFocus : ''}
-//   ${disabled ? s.containerDisabled : ''}
-//   ${error ? s.borderError : s.border}
-//   ${error && isFocused ? s.colorError : !error && isFocused ? s.containerFocus : s.colorDefault}
-// `;
-//
-//     const styles = `
-//     ${s.main}
-//     ${error && icon === 'classic' ? s.borderError : icon !== 'classic' ? s.borderBlack : s.borderGrey}
-//      `;
-//
-//     return (
-//
-//         <>
-//             <div style={{width: '100%', backgroundColor: 'black'}}>
-//                 {icon === "search" || icon === "password" ? (
-//                     <>
-//                         <div className={stylesContainer} onFocus={handleFocus} onBlur={handleBlur}>
-//                             {icon === "search" && <img src={searchPict} alt="searchIcon"/>}
-//                             <input
-//                                 type={showPassword? 'text': "password"}
-//                                 onChange={(e) => setX(e.currentTarget.value)}
-//                                 value={x}
-//                                 disabled={disabled}
-//                                 className={`${className} ${styles}`}
-//                                 {...rest}
-//                             />
-//                             {icon === "search" && x !== '' &&
-//                                 <img src={xPict} alt="x" className={s.x} onClick={() => setX('')}/>}
-//                             {icon === "password" &&
-//                                 <img src={passwordPict} alt="passwordIcon" className={disabled ? s.icon : ''}/>}
-//                         </div>
-//                         <Typography as="span" variant="caption" className={t.error}>{error}</Typography>
-//                     </>
-//
-//                 ) : (
-//                     <div style={{width: '284px'}} onFocus={handleFocus} onBlur={handleBlur}>
-//                         <Controller
-//                             name={name}
-//                             control={control}
-//                             // rules={rules}
-//                             // defaultValue={defaultValue}
-//                             render={({ field,fieldState }) => (
-//                                 <input
-//                                     {...field}
-//                                     disabled={disabled}
-//                                     className={`${className} ${styles}`}
-//                                     {...rest}
-//                                 />
-//                             )}
-//                         />
-//                         <Typography as="span" variant="caption" className={t.error}>{error}</Typography>
-//                     </div>
-//                 )}
-//             </div>
-//         </>
-//
-//     );
-// };
-
-
-//-----------------------------------------------------------------------------------
-
-// import searchPict from "@/components/ui/iconInput/assets/search.svg";
-// import xPict from "@/components/ui/iconInput/assets/x.svg";
-// import passwordPict from "@/components/ui/iconInput/assets/eye.svg";
-// import {useState} from "react";
-// import s from "./superInput.module.scss";
-// import t from "@/components/ui/typography/typography.module.scss";
-// import {Typography} from "@/components/ui/typography";
-// import {Control} from "react-hook-form";
-//
-// export type InputProps = {
-//     control: Control<any>;
-//     label?:string;
-//     className?: string;
-//     disabled?: boolean;
-//     icon?: "search" | "password" | "classic";
-// };
-//
-// export const SuperInput = (props: InputProps) => {
-//     const {className, disabled = true, icon = "search", ...rest} = props;
-//     const [isFocused, setIsFocused] = useState(false);
-//     const [error, setError] = useState<string | null>(null);
-//     //const [error, setError] = useState<string | null>('You have some Error')
-//     const [x, setX] = useState('');
-//     const[showPassword,setShowPassword]=useState(false)
-//
-//     const handleFocus = () => {
-//         setIsFocused(true);
-//     };
-//
-//     const handleBlur = () => {
-//         setIsFocused(false);
-//     };
-//
-//     const stylesContainer = `
-//   ${s.container}
-//   ${isFocused ? s.containerFocus : ''}
-//   ${disabled ? s.containerDisabled : ''}
-//   ${error ? s.borderError : s.border}
-//   ${error && isFocused ? s.colorError : !error && isFocused ? s.containerFocus : s.colorDefault}
-// `;
-//
-//     const styles = `
-//     ${s.main}
-//     ${error && icon === 'classic' ? s.borderError : icon !== 'classic' ? s.borderBlack : s.borderGrey}
-//      `;
-//
-//     return (
-//         <div style={{width: '100%', backgroundColor: 'black'}}>
-//             {icon === "search" || icon === "password" ? (
-//                 <>
-//                     <div className={stylesContainer} onFocus={handleFocus} onBlur={handleBlur}>
-//                         {icon === "search" && <img src={searchPict} alt="searchIcon"/>}
-//                         <input
-//                             type={showPassword? 'text': "password"}
-//                             onChange={(e) => setX(e.currentTarget.value)}
-//                             value={x}
-//                             disabled={disabled}
-//                             className={`${className} ${styles}`}
-//                             {...rest}
-//                         />
-//                         {icon === "search" && x !== '' &&
-//                             <img src={xPict} alt="x" className={s.x} onClick={() => setX('')}/>}
-//                         {icon === "password" &&
-//                             <img src={passwordPict} alt="passwordIcon" className={disabled ? s.icon : ''}/>}
-//                     </div>
-//                     <Typography as="span" variant="caption" className={t.error}>{error}</Typography>
-//                 </>
-//
-//             ) : (
-//                 <div style={{width: '284px'}} onFocus={handleFocus} onBlur={handleBlur}>
-//                     <input
-//                         disabled={disabled}
-//                         className={`${className} ${styles}`}
-//                         {...rest}
-//                     />
-//                     <Typography as="span" variant="caption" className={t.error}>{error}</Typography>
-//                 </div>
-//             )}
-//         </div>
-//     );
-// };
