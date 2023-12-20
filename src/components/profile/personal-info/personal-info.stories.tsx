2f8f0f165085486e0f4a997c9ce0e35b71134f50
IgorYudintsev <kakvospitat@gmail.com>
2023-12-20 11:33:02 +0300

    68 personalInfo

---
+++ src/components/profile/personal-info/personal-info.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { PersonalInfo } from '.'
+
+const meta = {
+    title: 'Profile/PersonalInfo',
+    component: PersonalInfo,
+    tags: ['autodocs'],
+} satisfies Meta<typeof PersonalInfo>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const EmptyAvatar: Story = {
+    args: {
+        name: 'Igor Y',
+        email: 'xxx@gmail.com',
+    },
+}
+export const NormalAvatar: Story = {
+    args: {
+        name: 'Igor Y',
+        email: 'xxx@gmail.com',
+        avatarSrc: 'https://avatars.githubusercontent.com/u/45824373?s=400&u=4c710a41dc877dcc3a2a9ddf5aecdfdc0fa1786d&v=4',
+    },
+}
+export const BrokenAvatar: Story = {
+    args: {
+        name: 'Igor Y',
+        email: 'xxx@gmail.com',
+        avatarSrc: 'https://avat.com/agsdfgsdfghshjgksdjfg',
+    },
+}
--- src/components/ui/avatar/avatar.tsx
+++ src/components/ui/avatar/avatar.tsx
@@ -1,12 +1,14 @@
-import { useEffect, useState } from 'react'
+import {ComponentProps, useEffect, useState} from 'react'
 import style from './avatar.module.scss'
+import clsx from 'clsx'
 
 type AvatarProps = {
-    src?: string
+    // src?: ComponentProps<'img'>['src']
     name: string
-}
+    // className: ComponentProps<'img'>['className']
+} & ComponentProps<'img'>
 
-export const Avatar = ({ src, name }: AvatarProps) => {
+export const Avatar = ({src, name, className}: AvatarProps) => {
     const [isAvaBroken, setIsAvaBroken] = useState(false)
 
     const errorHandler = () => {
@@ -17,15 +19,35 @@ export const Avatar = ({ src, name }: AvatarProps) => {
         setIsAvaBroken(false)
     }, [src])
 
+    const classNames = {
+        containerAvaBroken: clsx(style.avatarBroken, style.standartSize, className),
+        textInAvaBroken: clsx(style.initialsBroken),
+
+        // buttonWrapper: clsx(s.buttonWrapper, disabled && s.disabled, position === 'left' && s.left),
+        img: clsx(style.avatar, style.standartSize, className),
+    }
+
     return isAvaBroken || !src ? (
-        <AvatarBroken name={name} />
+        <AvatarBroken
+            name={name}
+            containerAvaBrokenStyle={classNames.containerAvaBroken}
+            textInAvaBrokenStyle={classNames.textInAvaBroken}
+        />
     ) : (
-        <img className={style.avatar} src={src} alt={name + ' avatar'} onError={errorHandler} />
+        <img className={classNames.img} src={src} alt={name + ' avatar'} onError={errorHandler}/>
     )
 }
 
-type AvatarBrokenProps = { name: string }
-const AvatarBroken = ({ name }: AvatarBrokenProps) => {
+type AvatarBrokenProps = {
+    name: string
+    containerAvaBrokenStyle: string
+    textInAvaBrokenStyle: string
+}
+const AvatarBroken = ({
+                          name,
+                          containerAvaBrokenStyle,
+                          textInAvaBrokenStyle,
+                      }: AvatarBrokenProps) => {
     const initials = name.substring(0, 2)
 
     // Генерируем уникальный цвет на основе имени
@@ -41,8 +63,51 @@ const AvatarBroken = ({ name }: AvatarBrokenProps) => {
 
     // Отрисовываем аватарку с заглушкой
     return (
-        <div className={style.avatarBroken} style={{ backgroundColor: color }}>
-            <span className={style.initialsBroken}>{initials}</span>
+        <div className={containerAvaBrokenStyle} style={{backgroundColor: color}}>
+            <span className={textInAvaBrokenStyle}>{initials}</span>
         </div>
     )
 }
+
+
+//---------------------------------
+// export const Avatar = ({ src, name,className }: AvatarProps) => {
+//     const [isAvaBroken, setIsAvaBroken] = useState(false)
+//
+//     const errorHandler = () => {
+//         setIsAvaBroken(true)
+//     }
+//
+//     useEffect(() => {
+//         setIsAvaBroken(false)
+//     }, [src])
+//
+//     return isAvaBroken || !src ? (
+//         <AvatarBroken name={name} />
+//     ) : (
+//         <img className={style.avatar} src={src} alt={name + ' avatar'} onError={errorHandler} />
+//     )
+// }
+//
+// type AvatarBrokenProps = { name: string }
+// const AvatarBroken = ({ name }: AvatarBrokenProps) => {
+//     const initials = name.substring(0, 2)
+//
+//     // Генерируем уникальный цвет на основе имени
+//     const hashCode = (s: string) => {
+//         return s.split('').reduce((a, b) => {
+//             a = (a << 5) - a + b.charCodeAt(0)
+//
+//             return a & a
+//         }, 0)
+//     }
+//
+//     const color = `hsl(${hashCode(name) % 360}, 50%, 50%)`
+//
+//     // Отрисовываем аватарку с заглушкой
+//     return (
+//         <div className={style.avatarBroken} style={{ backgroundColor: color }}>
+//             <span className={style.initialsBroken}>{initials}</span>
+//         </div>
+//     )
+// }
