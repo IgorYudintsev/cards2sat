58f182ec7f320c6b5d209bd9e990804a1a9eab5f
IgorYudintsev <kakvospitat@gmail.com>
2024-01-01 21:26:15 +0300

    71 dropDown

--- src/components/ui/drop-down-menu/drop-down-menu.stories.tsx
+++ src/components/ui/drop-down-menu/drop-down-menu.stories.tsx
@@ -1,22 +1,74 @@
-
 import type {Meta, StoryObj} from '@storybook/react'
-import DropDownMenu from "@/components/ui/drop-down-menu/drop-down-menu.tsx";
+import {DropDownMenu, DropDownMenuIcon} from './drop-down-menu'
+import {Button, Typography} from "@/components";
+import {Avatar} from "@/components/ui/avatar";
+import s from  './drop-down-menu.module.scss'
+import {PersonOutlineSvg} from "@/assets/icons/PersonOutlineSvg.tsx";
+import {CSSProperties} from "react";
 
 
 const meta = {
     title: 'Components/DropDownMenu',
     component: DropDownMenu,
-    argTypes: {
-        onSubmit: {action: 'submitted'},
-    },
+    // argTypes: {
+    //     onSubmit: {action: 'submitted'},
+    // },
     tags: ['autodocs'],
 } satisfies Meta<typeof DropDownMenu>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
+export const Primary: Story = {
 
-export const Primary: Story = {}
+    render: () => {
+        const primaryStyles: CSSProperties = {
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+        };
+
+        const wrapperForDropDownMenuIconStyles: CSSProperties = {
+            display: 'flex',
+            flexDirection: 'row',
+            gap: '0.5rem',
+            alignItems: 'center',
+        };
+
+        const wrapperForTypographyStyles: CSSProperties = {
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'flex-start',
+        };
+
+        return (
+            <div style={primaryStyles}>
+                <DropDownMenu trigger={<Button>open me</Button>}>
+                    <DropDownMenuIcon onSelect={() => {}}>
+                        <div style={wrapperForDropDownMenuIconStyles}>
+                            <Avatar name="Igor" />
+                            <div style={wrapperForTypographyStyles}>
+                                <Typography variant="subtitle2" as="span">
+                                    Igor
+                                </Typography>
+                                <Typography variant="caption" as="span">
+                                    dollarselephant@gmail.com
+                                </Typography>
+                            </div>
+                        </div>
+                    </DropDownMenuIcon>
+                    {/*<DropDownMenuIcon onSelect={() => {*/}
+                    {/*}} icon={<PersonOutlineSvg/>}>*/}
+                    {/*    My Profile*/}
+                    {/*</DropDownMenuIcon>*/}
+                    {/*<DropDownMenuIcon >Sign Out</DropDownMenuIcon>*/}
+                </DropDownMenu>
+            </div>
+        );
+    },
+};
+
+
 
 
 
@@ -93,3 +145,26 @@ export const Primary: Story = {}
 //         )
 //     },
 // }
+//----------------------------------------------------------------------------------
+
+// import type {Meta, StoryObj} from '@storybook/react'
+// import {DropDownMenu} from "@/components/ui/drop-down-menu/drop-down-menu.tsx";
+//
+//
+// const meta = {
+//     title: 'Components/DropDownMenu',
+//     component: DropDownMenu,
+//     argTypes: {
+//         onSubmit: {action: 'submitted'},
+//     },
+//     tags: ['autodocs'],
+// } satisfies Meta<typeof DropDownMenu>
+//
+// export default meta
+// type Story = StoryObj<typeof meta>
+//
+//
+// export const Primary: Story = {
+
+
+
--- src/components/ui/drop-down-menu/drop-down-menu.tsx
+++ src/components/ui/drop-down-menu/drop-down-menu.tsx
@@ -1,107 +1,195 @@
-import React from 'react';
-import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
-import {
-    HamburgerMenuIcon,
-    DotFilledIcon,
-    CheckIcon,
-    ChevronRightIcon,
-} from '@radix-ui/react-icons';
-//import './styles.css';
+import { ReactNode, useState, FC, CSSProperties } from 'react'
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
 
-const DropdownMenuDemo = () => {
-    const [bookmarksChecked, setBookmarksChecked] = React.useState(true);
-    const [urlsChecked, setUrlsChecked] = React.useState(false);
-    const [person, setPerson] = React.useState('pedro');
+
+import style from './drop-down-menu.module.scss'
+import {Typography} from "@/components";
+
+export type DropdownProps = {
+    children: ReactNode
+    align?: 'start' | 'center' | 'end'
+    trigger?: ReactNode
+    className?: string
+    style?: CSSProperties
+} & DropdownMenu.DropdownMenuProps
+
+export const DropDownMenu: FC<DropdownProps> = ({
+                                                    children,
+                                                    align,
+                                                    className,
+                                                    trigger,
+                                                    ...restProps
+                                                }) => {
+    const [open, setOpen] = useState(false)
 
     return (
-        <DropdownMenu.Root>
+        <DropdownMenu.Root onOpenChange={setOpen} modal>
             <DropdownMenu.Trigger asChild>
-                <button className="IconButton" aria-label="Customise options">
-                    <HamburgerMenuIcon />
-                </button>
+                <div style={{ height: 'fit-content', width: 'fit-content', position: 'relative' }}>
+                    {trigger}
+                    {open && <div className={style.DropdownMenuArrow3}></div>}
+                </div>
             </DropdownMenu.Trigger>
+            {/* forceMount Useful when controlling animation */}
 
-            <DropdownMenu.Portal>
-                <DropdownMenu.Content className="DropdownMenuContent" sideOffset={5}>
-                    <DropdownMenu.Item className="DropdownMenuItem">
-                        New Tab <div className="RightSlot">⌘+T</div>
-                    </DropdownMenu.Item>
-                    <DropdownMenu.Item className="DropdownMenuItem">
-                        New Window <div className="RightSlot">⌘+N</div>
-                    </DropdownMenu.Item>
-                    <DropdownMenu.Item className="DropdownMenuItem" disabled>
-                        New Private Window <div className="RightSlot">⇧+⌘+N</div>
-                    </DropdownMenu.Item>
-                    <DropdownMenu.Sub>
-                        <DropdownMenu.SubTrigger className="DropdownMenuSubTrigger">
-                            More Tools
-                            <div className="RightSlot">
-                                <ChevronRightIcon />
-                            </div>
-                        </DropdownMenu.SubTrigger>
-                        <DropdownMenu.Portal>
-                            <DropdownMenu.SubContent
-                                className="DropdownMenuSubContent"
-                                sideOffset={2}
-                                alignOffset={-5}
-                            >
-                                <DropdownMenu.Item className="DropdownMenuItem">
-                                    Save Page As… <div className="RightSlot">⌘+S</div>
-                                </DropdownMenu.Item>
-                                <DropdownMenu.Item className="DropdownMenuItem">Create Shortcut…</DropdownMenu.Item>
-                                <DropdownMenu.Item className="DropdownMenuItem">Name Window…</DropdownMenu.Item>
-                                <DropdownMenu.Separator className="DropdownMenu.Separator" />
-                                <DropdownMenu.Item className="DropdownMenuItem">Developer Tools</DropdownMenu.Item>
-                            </DropdownMenu.SubContent>
-                        </DropdownMenu.Portal>
-                    </DropdownMenu.Sub>
-
-                    <DropdownMenu.Separator className="DropdownMenuSeparator" />
-
-                    <DropdownMenu.CheckboxItem
-                        className="DropdownMenuCheckboxItem"
-                        checked={bookmarksChecked}
-                        onCheckedChange={setBookmarksChecked}
-                    >
-                        <DropdownMenu.ItemIndicator className="DropdownMenuItemIndicator">
-                            <CheckIcon />
-                        </DropdownMenu.ItemIndicator>
-                        Show Bookmarks <div className="RightSlot">⌘+B</div>
-                    </DropdownMenu.CheckboxItem>
-                    <DropdownMenu.CheckboxItem
-                        className="DropdownMenuCheckboxItem"
-                        checked={urlsChecked}
-                        onCheckedChange={setUrlsChecked}
-                    >
-                        <DropdownMenu.ItemIndicator className="DropdownMenuItemIndicator">
-                            <CheckIcon />
-                        </DropdownMenu.ItemIndicator>
-                        Show Full URLs
-                    </DropdownMenu.CheckboxItem>
-
-                    <DropdownMenu.Separator className="DropdownMenuSeparator" />
-
-                    <DropdownMenu.Label className="DropdownMenuLabel">People</DropdownMenu.Label>
-                    <DropdownMenu.RadioGroup value={person} onValueChange={setPerson}>
-                        <DropdownMenu.RadioItem className="DropdownMenuRadioItem" value="pedro">
-                            <DropdownMenu.ItemIndicator className="DropdownMenuItemIndicator">
-                                <DotFilledIcon />
-                            </DropdownMenu.ItemIndicator>
-                            Pedro Duarte
-                        </DropdownMenu.RadioItem>
-                        <DropdownMenu.RadioItem className="DropdownMenuRadioItem" value="colm">
-                            <DropdownMenu.ItemIndicator className="DropdownMenuItemIndicator">
-                                <DotFilledIcon />
-                            </DropdownMenu.ItemIndicator>
-                            Colm Tuite
-                        </DropdownMenu.RadioItem>
-                    </DropdownMenu.RadioGroup>
-
-                    <DropdownMenu.Arrow className="DropdownMenuArrow" />
+            <DropdownMenu.Portal className={style.portal}>
+                <DropdownMenu.Content
+                    className={style.DropdownMenuContent}
+                    align="start"
+                    sideOffset={5}
+                    hideWhenDetached
+                >
+                    {children}
+                    {/* <DropdownMenu.Arrow className={style['DropdownMenuArrow']} /> */}
+                    {/* <div className={style.DropdownMenuArrow2}></div> */}
                 </DropdownMenu.Content>
             </DropdownMenu.Portal>
         </DropdownMenu.Root>
-    );
-};
+    )
+}
 
-export default DropdownMenuDemo;
+type DropDownMenuIconProps = {
+    children: ReactNode
+    icon?: ReactNode
+} & DropdownMenu.DropdownMenuItemProps
+
+export const DropDownMenuIcon = ({
+  onSelect,
+  disabled,
+  className,
+  children,
+  icon,
+  ...restProps
+}: DropDownMenuIconProps) => {
+
+  return (
+    <>
+      <DropdownMenu.Item
+        onSelect={onSelect}
+        // className={className}
+        className={style.DropdownMenuItem}
+        disabled={disabled}
+        {...restProps}
+      >
+        {icon}
+        <Typography variant="subtitle2" style={{ margin: 0 }}>
+          {children}
+        </Typography>
+      </DropdownMenu.Item>
+    </>
+  )
+}
+
+
+//----------------------------------------------------------------------------
+
+// import React, {useState} from 'react';
+// import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
+// import {
+//     HamburgerMenuIcon,
+//     DotFilledIcon,
+//     CheckIcon,
+//     ChevronRightIcon,
+// } from '@radix-ui/react-icons';
+// //import './styles.css';
+//
+// export const DropDownMenu = () => {
+//     const [bookmarksChecked, setBookmarksChecked] = React.useState(true);
+//     const [urlsChecked, setUrlsChecked] = React.useState(false);
+//     const [person, setPerson] = React.useState('pedro');
+//
+//     const [open, setOpen] = useState(false)
+//
+//     return (
+//
+//
+//         <DropdownMenu.Root onOpenChange={setOpen} modal>
+//             <DropdownMenu.Trigger asChild>
+//                 <button className="IconButton" aria-label="Customise options">
+//                     <HamburgerMenuIcon />
+//                 </button>
+//             </DropdownMenu.Trigger>
+//
+//             <DropdownMenu.Portal>
+//                 <DropdownMenu.Content className="DropdownMenuContent" sideOffset={5}>
+//                     <DropdownMenu.Item className="DropdownMenuItem">
+//                         New Tab <div className="RightSlot">⌘+T</div>
+//                     </DropdownMenu.Item>
+//                     <DropdownMenu.Item className="DropdownMenuItem">
+//                         New Window <div className="RightSlot">⌘+N</div>
+//                     </DropdownMenu.Item>
+//                     <DropdownMenu.Item className="DropdownMenuItem" disabled>
+//                         New Private Window <div className="RightSlot">⇧+⌘+N</div>
+//                     </DropdownMenu.Item>
+//                     <DropdownMenu.Sub>
+//                         <DropdownMenu.SubTrigger className="DropdownMenuSubTrigger">
+//                             More Tools
+//                             <div className="RightSlot">
+//                                 <ChevronRightIcon />
+//                             </div>
+//                         </DropdownMenu.SubTrigger>
+//                         <DropdownMenu.Portal>
+//                             <DropdownMenu.SubContent
+//                                 className="DropdownMenuSubContent"
+//                                 sideOffset={2}
+//                                 alignOffset={-5}
+//                             >
+//                                 <DropdownMenu.Item className="DropdownMenuItem">
+//                                     Save Page As… <div className="RightSlot">⌘+S</div>
+//                                 </DropdownMenu.Item>
+//                                 <DropdownMenu.Item className="DropdownMenuItem">Create Shortcut…</DropdownMenu.Item>
+//                                 <DropdownMenu.Item className="DropdownMenuItem">Name Window…</DropdownMenu.Item>
+//                                 <DropdownMenu.Separator className="DropdownMenu.Separator" />
+//                                 <DropdownMenu.Item className="DropdownMenuItem">Developer Tools</DropdownMenu.Item>
+//                             </DropdownMenu.SubContent>
+//                         </DropdownMenu.Portal>
+//                     </DropdownMenu.Sub>
+//
+//                     <DropdownMenu.Separator className="DropdownMenuSeparator" />
+//
+//                     <DropdownMenu.CheckboxItem
+//                         className="DropdownMenuCheckboxItem"
+//                         checked={bookmarksChecked}
+//                         onCheckedChange={setBookmarksChecked}
+//                     >
+//                         <DropdownMenu.ItemIndicator className="DropdownMenuItemIndicator">
+//                             <CheckIcon />
+//                         </DropdownMenu.ItemIndicator>
+//                         Show Bookmarks <div className="RightSlot">⌘+B</div>
+//                     </DropdownMenu.CheckboxItem>
+//                     <DropdownMenu.CheckboxItem
+//                         className="DropdownMenuCheckboxItem"
+//                         checked={urlsChecked}
+//                         onCheckedChange={setUrlsChecked}
+//                     >
+//                         <DropdownMenu.ItemIndicator className="DropdownMenuItemIndicator">
+//                             <CheckIcon />
+//                         </DropdownMenu.ItemIndicator>
+//                         Show Full URLs
+//                     </DropdownMenu.CheckboxItem>
+//
+//                     <DropdownMenu.Separator className="DropdownMenuSeparator" />
+//
+//                     <DropdownMenu.Label className="DropdownMenuLabel">People</DropdownMenu.Label>
+//                     <DropdownMenu.RadioGroup value={person} onValueChange={setPerson}>
+//                         <DropdownMenu.RadioItem className="DropdownMenuRadioItem" value="pedro">
+//                             <DropdownMenu.ItemIndicator className="DropdownMenuItemIndicator">
+//                                 <DotFilledIcon />
+//                             </DropdownMenu.ItemIndicator>
+//                             Pedro Duarte
+//                         </DropdownMenu.RadioItem>
+//                         <DropdownMenu.RadioItem className="DropdownMenuRadioItem" value="colm">
+//                             <DropdownMenu.ItemIndicator className="DropdownMenuItemIndicator">
+//                                 <DotFilledIcon />
+//                             </DropdownMenu.ItemIndicator>
+//                             Colm Tuite
+//                         </DropdownMenu.RadioItem>
+//                     </DropdownMenu.RadioGroup>
+//
+//                     <DropdownMenu.Arrow className="DropdownMenuArrow" />
+//                 </DropdownMenu.Content>
+//             </DropdownMenu.Portal>
+//         </DropdownMenu.Root>
+//     );
+// };
+
