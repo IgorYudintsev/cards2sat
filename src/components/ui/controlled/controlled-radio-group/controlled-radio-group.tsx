16de8cea52056d5cf0317ed019342dc0740c7eb9
IgorYudintsev <kakvospitat@gmail.com>
2024-01-04 21:51:37 +0300

    74 RadioGroupControlled

--- src/components/ui/controlled/controlled-radio-group/controlled-radio-group.tsx
+++ src/components/ui/controlled/controlled-radio-group/controlled-radio-group.tsx
@@ -1,9 +1,34 @@
-import {RadioGroup} from "@/components/ui/radio-group";
+import {RadioGroup, RadioGroupProps} from "@/components/ui/radio-group";
+import {FieldValues, useController, UseControllerProps} from "react-hook-form";
 
-export const ControlledRadioGroup = () => {
+type ControlledRadioGroupProps<T extends FieldValues> = UseControllerProps<T> &
+    Omit<RadioGroupProps, 'onValueChange' | 'name' | 'defaultValue'>
+
+export const ControlledRadioGroup = <T extends FieldValues>({
+                                                           control,
+                                                           name,
+                                                           defaultValue,
+                                                           rules,
+                                                           shouldUnregister,
+                                                           ...radioGroupProps
+                                                       }: ControlledRadioGroupProps<T>) => {
+    const {
+        field: { onChange },
+    } = useController({
+        control,
+        name,
+        defaultValue,
+        rules,
+        shouldUnregister,
+    })
+
     return (
-        <div></div>
-        // <RadioGroup/>
-    );
-};
+        <RadioGroup
+            defaultValue={defaultValue}
+            onValueChange={onChange}
+            id={name}
+            {...radioGroupProps}
+        />
 
+    )
+}
--- src/components/ui/radio-group/radio-group.stories.tsx
+++ src/components/ui/radio-group/radio-group.stories.tsx
@@ -1,9 +1,9 @@
-import { DevTool } from '@hookform/devtools'
 import type { Meta, StoryObj } from '@storybook/react'
-import { useForm } from 'react-hook-form'
 import {RadioGroup} from "@/components/ui/radio-group/radio-group.tsx";
+import {useForm} from "react-hook-form";
+import {DevTool} from "@hookform/devtools";
+import {ControlledRadioGroup} from "@/components/ui/controlled/controlled-radio-group/controlled-radio-group.tsx";
 import {Button} from "@/components";
-
 
 const meta = {
     title: 'Components/RadioGroup',
@@ -54,37 +54,37 @@ export const Disabled: Story = {
 type formType = {
     isKnow: string
 }
-// export const RadioControll = {
-//     render: () => {
-//         const options = [
-//             {
-//                 label: '1',
-//                 value: '1',
-//             },
-//             {
-//                 label: '2',
-//                 value: '2',
-//             },
-//             {
-//                 label: '3',
-//                 value: '3',
-//             },
-//         ]
-//         const { handleSubmit, control } = useForm<formType>({
-//             defaultValues: {
-//                 isKnow: '3',
-//             },
-//         })
-//         const onSubmit = (data: formType) => {
-//             alert('you choose=>>> ' + data.isKnow)
-//         }
-//
-//         return (
-//             <form onSubmit={handleSubmit(onSubmit)}>
-//                 <DevTool control={control} />
-//                 <ControlledRadio name={'isKnow'} options={options} control={control} defaultValue={'3'} />
-//                 <Button type={'submit'}>Submit</Button>
-//             </form>
-//         )
-//     },
-// }
+export const RadioControll = {
+    render: () => {
+        const options = [
+            {
+                label: '1',
+                value: '1',
+            },
+            {
+                label: '2',
+                value: '2',
+            },
+            {
+                label: '3',
+                value: '3',
+            },
+        ]
+        const { handleSubmit, control } = useForm<formType>({
+            defaultValues: {
+                isKnow: '3',
+            },
+        })
+        const onSubmit = (data: formType) => {
+            alert('you choose=>>> ' + data.isKnow)
+        }
+
+        return (
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <DevTool control={control} />
+                <ControlledRadioGroup name={'isKnow'} options={options} control={control} defaultValue={'3'} />
+                <Button type={'submit'}>Submit</Button>
+            </form>
+        )
+    },
+}
