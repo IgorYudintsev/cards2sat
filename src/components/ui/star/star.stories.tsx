d2012bc5a882602ab0f44e591340cc4bfc33e90f
IgorYudintsev <kakvospitat@gmail.com>
2024-01-06 21:36:16 +0300

    78 star

---
+++ src/components/ui/star/star.stories.tsx
@@ -0,0 +1,19 @@
+import type {Meta, StoryObj} from '@storybook/react'
+
+import {StarRating} from '.'
+
+const meta = {
+    title: 'Components/Star',
+    component: StarRating,
+    tags: ['autodocs'],
+} satisfies Meta<typeof StarRating>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+    args: {
+        rating: 0,
+        setLengthRating: 5,
+    },
+}
---
+++ src/components/ui/star/star.tsx
@@ -0,0 +1,16 @@
+import { StarSVG } from '@/assets/icons/StarSvg.tsx'
+
+type StarRatingProps = {
+    rating: number
+    setLengthRating: number
+}
+
+export const StarRating = (props: StarRatingProps) => {
+    return (
+        <div>
+            {Array.from({ length: props.setLengthRating }, (_, i) => (
+                <StarSVG starId={i + 1} key={`star_${i + 1} `} marked={++i <= props.rating} />
+            ))}
+        </div>
+    )
+}
